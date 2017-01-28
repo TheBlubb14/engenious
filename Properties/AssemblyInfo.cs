d15952ea043d3c2687797fff1f40502d50977e15
jvbsl <jvbsl@novacyb.com>
2017-01-28 19:00:19 +0100

    added certificate for engenious and signed the target assemblies

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,10 +14,9 @@ using System.Runtime.CompilerServices;
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
 
-[assembly:InternalsVisibleTo("ContentTool")]
-[assembly:InternalsVisibleTo("engenious.Pipeline")]
-[assembly:InternalsVisibleTo("Sample")]
-[assembly:InternalsVisibleTo("XNA.BLEND")]
+[assembly:InternalsVisibleTo("ContentTool, PublicKey=00240000048000009400000006020000002400005253413100040000010001009bf5fc6c095678b8fb8a322fdc4ac8a47b93063491e511176a0715e69bd565c0e7998cb60b3ade555946b5e65e62ba318a06872801170b4bf972fa91b7120e8b823ed1c8e99f083cb47c853ef2ef64dff47813d54b0bce15d1f580a1f723815c561461d4eda70bd644a21110f90372cde16a3501c1c45da92d02cd0f5ca096a0")]
+[assembly:InternalsVisibleTo("engenious.Pipeline, PublicKey=00240000048000009400000006020000002400005253413100040000010001009bf5fc6c095678b8fb8a322fdc4ac8a47b93063491e511176a0715e69bd565c0e7998cb60b3ade555946b5e65e62ba318a06872801170b4bf972fa91b7120e8b823ed1c8e99f083cb47c853ef2ef64dff47813d54b0bce15d1f580a1f723815c561461d4eda70bd644a21110f90372cde16a3501c1c45da92d02cd0f5ca096a0")]
+
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
